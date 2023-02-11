f99ef740e8c098d9551f3ea15e2ff1949b6962c5
badradine <124945730+badradine@users.noreply.github.com>
2023-02-11 15:59:04 -0500

    Add files via upload

--- ali lab1.py
+++ ali lab1.py
@@ -3,9 +3,19 @@ y=int(input("enter the value of b: "))
 z=int(input("enter the value of c: "))
 
 import math
-s = math.sqrt((y**2)-(4*x*z))
+p=((y**2)-(4*x*z))
 
-a1 = (-y+s)/(2*x)
-a2 = (-y-s)/(2*x)
+if p==0:
+    s = math.sqrt(p)
+    a1 = (-y + s) / (2 * x)
+    a2 = (-y - s) / (2 * x)
+    print("The value of x is: ", a1)
+elif p<0:
+    print("no result")
+else:
+    s = math.sqrt(p)
+    a1 = (-y + s) / (2 * x)
+    a2 = (-y - s) / (2 * x)
+    print("the roots of a are: ", a1, "and", a2)
 
-print("the roots of a are:", a1, "and", a2)
+
